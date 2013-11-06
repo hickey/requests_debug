167bde8c2943b2dae40173aa3d744f402f80025c
Eric Moritz <eric@themoritzfamily.com>
2013-11-06 13:18:56 -0500

    Added my info

--- setup.py
+++ setup.py
@@ -6,4 +6,7 @@ setup(
     name="requests_debug",
     description="Adds logging and timing for the requests library",
     version=requests_debug.__version__,
+    url="https://github.com/ericmoritz/requests_debug",
+    author="Eric Moritz",
+    author_email="eric@themoritzfamily.com",
     py_modules="requests_debug")
